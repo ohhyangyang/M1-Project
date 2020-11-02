95a6e91477191d54f9dd02ebdbeeaccb56e9dbd9
Yang Ge <holayangge@gmail.com>
2020-11-02 13:33:47 +0100

    update links to index.html, add QueryFilter.js and update it, and update ApiService.js for fetch

--- scripts/ApiService.js
+++ scripts/ApiService.js
@@ -1,36 +1,52 @@
-const urlParams = new URLSearchParams(window.location.search);
-console.log(urlParams)
-const myParam = urlParams.get('product_type');
-console.log(myParam)
+// class QueryFilter {
+//   constructor() {
+//     this.urlParams = new URLSearchParams(window.location.search);
+//     this.typeParam = this.urlParams.get("product_type");
+//     this.brandParam = this.urlParams.get("brand");
+//   }
 
-const productsList = document.querySelector('.products-container');
-console.log(productsList)
-    // Make a request to the server (API) using the URL
-    
-    // fetch(`http://makeup-api.herokuapp.com/api/v1/products.json?product_type=lipstick`)
-    // .then( (response) => {
-    //   console.log('response', response);
-  
-    //   // converts the response.body to a JS object
-    //   return response.json();  
-    // })
-    // .then((data) => {
-    //   console.log('data', data)
-    //   data.forEach((el)=>{
-          
-    //       const div = document.createElement('div');
-    //       div.innerHTML =
-    //       `
-    //       <img src="${el.image_link}" />
-    //       `
+//   getQuery() {
+//     if (this.typeParam) {
+//       return `product_type=${this.typeParam}`;
+//     } else if (this.urlParams) {
+//       return `${this.urlParams}`;
+//     }
+//   }
 
-    //       document.body.append(div)
-    //   })
+//   test() {
+//     console.log(this.typeParam);
+//     console.log(this.brandParam);
+//   }
+// }
 
-      
-  
-  
+// const queryFilter = new QueryFilter();
+console.log(queryFilter.getQuery());
 
-  
-    // })
-    
+//get queryStr
+const queryStr = queryFilter.getQuery();
+
+//get productList wrapper
+const productsList = document.querySelector(".products-container");
+console.log(productsList);
+
+//proxyUrl阻止safari的blocker
+let proxyUrl = "https://cors-anywhere.herokuapp.com/";
+//开始fetch
+fetch(proxyUrl + `http://makeup-api.herokuapp.com/api/v1/products.json?${queryStr}`)
+  .then((response) => {
+    console.log("response", response);
+
+    // converts the response.body to a JS object
+    return response.json();
+  })
+  .then((data) => {
+    console.log("data", data);
+    data.forEach((el) => {
+      if (el.image_link) {
+        const div = document.createElement("div");
+        div.innerHTML = `<img src="${el.image_link}" />`;
+
+        productsList.append(div);
+      }
+    });
+  });
